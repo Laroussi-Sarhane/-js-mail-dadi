1fade1672ee064ea5da5fb8f22a007bddf3907c6
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-02-23 15:05:14 +0100

    add changes

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const listaMail = []
 const emailUtente = prompt('inserisci qui la tua mail')
+for (let i=0; i < emailUtente; i++){
+  const email = prompt('inserisci email')
+  listaMail.push(email);
+
+}
+for (let i=0; i< array.length; i++ ) {
+  const element = array[i];
+}
+console.log(listaMail);
